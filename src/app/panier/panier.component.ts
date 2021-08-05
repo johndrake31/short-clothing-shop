af9715c51c619f279a7ec41760b449f0b46927dc
Peterturn <55192090+Peterturn@users.noreply.github.com>
2021-08-05 11:36:41 +0200

    added a sort function to reduce duplicates but add a qty to replace duplicate lines

--- src/app/panier/panier.component.ts
+++ src/app/panier/panier.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -8,8 +8,15 @@ import { CartService } from '../cart.service';
   styleUrls: ['./panier.component.css']
 })
 
+
+
+
 export class PanierComponent implements OnInit {
+
+
   basket: any = []
+  organizedBasket: any = [];
+  disOrganizedBasket: any = [];
 
   totalPrice() {
     let total = 0
@@ -21,20 +28,56 @@ export class PanierComponent implements OnInit {
     return total
   };
 
+  // for duplicates how to make it one with a count of items
 
   removeFromBasket(short: any) {
     this.basketServ.remove(short)
   }
 
-  @Input() short: any = {};
 
   constructor(private http: HttpClient, private basketServ: CartService) {
-
+    this.basket = this.basketServ.getBasket();
   }
 
   ngOnInit(): void {
-    this.basket = this.basketServ.getBasket();
+    this.sortBasket();
   }
+
+
+
+  sortBasket() {
+    let unSortedCart = this.basket;
+    let sortedBasket: any = []
+
+    unSortedCart.forEach((shortInChaos: any) => {
+      // console.log(shortInChaos);
+      // {id: 2, name: "nike basket"...}
+
+      let indexPossible = sortedBasket.findIndex((sortedShort: any) => sortedShort.id == shortInChaos.id)
+      // console.log(indexPossible);
+      // -1 tout temps
+      if (indexPossible >= 0) {
+
+        sortedBasket[indexPossible].qty++
+
+      } else {
+        let newShort = {
+          id: shortInChaos.id,
+          name: shortInChaos.name,
+          size: shortInChaos.size,
+          color: shortInChaos.color,
+          price: shortInChaos.price,
+          qty: 1
+        }
+        sortedBasket.push(newShort);
+      }
+    });
+
+    console.log(sortedBasket);
+
+    return this.basket = sortedBasket;
+  }
+
 }
 
   // addNewShortPlease(name: string, color: string, size: string) {
